3664461eedf47a8b1b5965e5a758dd5cf4fb97a4
Alexander <Alexander-e-mail@yandex.ru>
2019-08-06 16:26:35 +0300

    Add Abstract Factory

---
+++ AbstractFactoryPattern/IAbstractProductB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactoryPattern
+{
+    public interface IAbstractProductB
+    {
+        string UsefulFunctionB();
+        string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+    }
+}
